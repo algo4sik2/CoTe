9dd83305ab1882d212d236b058bf033dce9e687f
Raziel-JKM <Jeongkm0127@kakao.com>
2021-12-06 23:54:53 +0900

    Update implement_km.py

--- implement/implement_km.py
+++ implement/implement_km.py
@@ -22,3 +22,33 @@ if summary = = 0 :
 
 else:
     print ("READY")
+    
+    --------------------------------------
+    -----------------------------------------
+    
+    
+    def solution (s):
+    answer = len(s)
+
+    for step in range(1, len(s) // 2 + 1)
+
+        compressed = "" 
+
+        prev = s[0:step] 
+        count = 1
+
+    for j in range(step, len(s), step):
+
+        count += 1
+
+    else
+
+        compressed += str(count ) + prev 
+        prev = s[j :j + step] 
+        count = 1
+
+    compressed += str(count) + prev if count >= 2 else prev
+
+    answer = min (answer, len(compressed))
+
+return answer
