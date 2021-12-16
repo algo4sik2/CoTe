162b481dca700d9b4dd3834934ac38b2d8348038
taekkim <taekkim@student.42seoul.kr>
2021-12-16 23:36:05 +0900

    Taekyun Kim

--- search/taekkim.py
+++ search/taekkim.py
@@ -1,24 +1,24 @@
 import timeit
 
 def ttoboki():
-		n, m = map(int, input().split())
-		d = sorted(map(int, input().split()), reverse=True)
-		i = 0
-		d_max = d[i]
-		while d_max > 0:
-				d_sum = 0
-				for x in d:
-						if x > d_max:
-								d_sum += x - d_max
-								print(x, d_sum)
-						else:
-								break
-				if d_sum >= m:
-						print(d_max)
-						return d_max
-				else:
-						d_max -= 1
+	n, m = map(int, input().split())
+	d = sorted(map(int, input().split()), reverse=True)
+	i = 0
+	d_max = d[i]
+	while d_max > 0:
+		d_sum = 0
+		for x in d:
+			if x > d_max:
+				d_sum += x - d_max	
+				print(x, d_sum)
+			else:	
+				break
+		if d_sum >= m:
+			print(d_max)
+			return d_max
+		else:
+			d_max -= 1
 
-t1 = timeit.timeit('ttoboki()', setup='from __main__ import ttoboki')
-print(t1)
+# t1 = timeit.timeit('ttoboki()', setup='from __main__ import ttoboki')
+# print(t1)
 
