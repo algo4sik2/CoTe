c96878444e1c37608df55fe5e904040cf42611f4
Jun Young Yoon <jjun9288@gmail.com>
2021-12-17 01:48:50 +0900

    Jun Young Yoon

--- search/jjun8177.py
+++ search/jjun8177.py
@@ -1,6 +1,46 @@
-# 높이 h 지정. h보다 긴 떡은 h 윗부분 절단, 낮은 부분은 안 자름
-# h = 15, in = [19, 14, 10, 17] -> [4, 0, 0, 2] -> 6
+# 5-2
 
+n = int(input())
+me = list(map(int, input().split()))
+
+m = int(input())
+you = list(map(int, input().split()))
+
+me.sort()
+
+answer = []
+
+def bs(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+    
+        if array[mid] == target:
+            return mid
+    
+        elif array[mid] > target:
+            end = mid - 1
+        
+        else:
+            start = mid + 1
+        
+        return None   
+
+    
+for i in you:
+    result = bs(me, i, 0, m-1)
+    
+    if result == None:
+        answer.append('no')
+    else:
+        answer.append('yes')
+        
+for i in answer:
+    print(i, end = ' ')
+    
+    
+
+
+#5-3
 n, m = map(int, input().split())
 tteok = list(map(int, input().split()))
 
