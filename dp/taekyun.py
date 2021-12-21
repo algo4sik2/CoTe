20c47ce5e9f11b340acf6a877305b2116cc9f2d5
taekkim <taekkim@student.42seoul.kr>
2021-12-21 10:50:29 +0900

    Taekyun Kim

--- dp/taekyun.py
+++ dp/taekyun.py
@@ -1,20 +1,20 @@
 # 2
 
-# n = int(input())
-# dp = [0] * (n + 1)
-# for i in range(2, n + 1):
-#     dp[i] = dp[i - 1] + 1
-#     if i % 5 == 0:
-#         print(i, dp)
-#         print(dp[i // 5], dp[i - 1])
-#         dp[i] = min(dp[i // 5], dp[i - 1]) + 1
-#         print(i, dp)
-#     if i % 3 == 0:
-#         dp[i] = min(dp[i // 3], dp[i - 1]) + 1        
-#     if i % 2 == 0:
-#         dp[i] = min(dp[i // 2], dp[i - 1]) + 1
+n = int(input())
+dp = [0] * (n + 1)
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+    if i % 5 == 0:
+        print(i, dp)
+        print(dp[i // 5], dp[i - 1])
+        dp[i] = min(dp[i // 5], dp[i - 1]) + 1
+        print(i, dp)
+    if i % 3 == 0:
+        dp[i] = min(dp[i // 3], dp[i - 1]) + 1        
+    if i % 2 == 0:
+        dp[i] = min(dp[i // 2], dp[i - 1]) + 1
 
-# print(dp[n])
+print(dp[n])
 
 # 3
 # a_2 = a_2 + a_0, a_new = a + max(a_n-2 + a_n-3)
